292fea1a69d5d0f71036741b41a9801ed135db4c
iso suluk <dede.suluk.1970@gmail.com>
2021-01-20 23:50:42 +0300

    20 ocak ders sonu

---
+++ Genericsİntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Genericsİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin Demirog");
+
+
+        }
+    }
+}
